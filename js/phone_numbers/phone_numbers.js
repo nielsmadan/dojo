e682f47c8f986a3ab0f8d06f40ff66c06a131899
nielsmadan <nielsmadan@gmail.com>
2013-04-12 08:56:48 +0200

    js: partial phone numbers solution.

--- js/phone_numbers/phone_numbers.js
+++ js/phone_numbers/phone_numbers.js
@@ -1,10 +1,70 @@
 module.exports = {
-    square: function(x) {
-        return _square(x);
+    startsWith: function(target, prefix) {
+        return _startsWith(target, prefix);
+    },
+
+    isValidPhoneNumber: function (phoneNumber) {
+    	return _isValidPhoneNumber(phoneNumber);
+    },
+
+    isConsistentNumberList: function (validNumberList) {
+    	return _isConsistentNumberList(validNumberList);
+    },
+
+    partitionList: function (validNumberList) {
+    	return _partitionList(validNumberList);
+    },
+
+    getEmptyPartitionedList: function () {
+    	return _getEmptyPartitionedList();
     }
 }
 
-function _square(x) {
-    return x * x;
+function _startsWith(target, prefix) {	
+    if(target.substring(0, prefix.length) === prefix) {
+    	return true;
+    } else {
+    	return false;
+    }
+}
+
+function _getEmptyPartitionedList() {
+	return [[], [], [], [], [], [], [], [], [], []];
+}
+
+
+var numList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function _partitionList(list) {
+	var partionedList = _getEmptyPartitionedList();
+    for (var i = 0; i < list.length; ++i) {
+    	var index = list[i][0];
+    	partionedList[index].push(list[i]);
+    }
+
+    return partionedList.filter(function (elem) { return elem.length != 0;});
+}
+
+function _isConsistentNumberList(validNumberList) {
+	for(var i=0; i < validNumberList.length; ++i)
+	{
+		for (var j=i+1; j < validNumberList.length; ++j) {
+			if (_startsWith(validNumberList[i], validNumberList[j]) ||
+				_startsWith(validNumberList[j], validNumberList[i])) {
+				return false;
+			}
+		}
+	}
+	
+	return true;
+}
+
+function _isValidPhoneNumber(phoneNumber) {
+	if (phoneNumber.match(/[A-Za-z]+/)) {
+		return false;
+	}
+	if (phoneNumber.match(/\d+/)) {
+		return true;
+	}
 }
 
--- js/phone_numbers/test_phone_numbers.js
+++ js/phone_numbers/test_phone_numbers.js
@@ -1,8 +1,60 @@
 var pn = require('./phone_numbers');
 var assert = require("assert")
 
-describe('square', function(){
-    it('should return 25 when the argument is 5', function(){
-      assert.equal(pn.square(5), 25);
-    })
-})
+describe('startsWith', function() {
+    it('should return true when the first argument is "1" and the second is also "1"', function(){
+      assert.equal(pn.startsWith("1", "1"), true);
+    });
+    it('should return false when the first argument is "1" and the second is also "2"', function(){      
+    	assert.equal(pn.startsWith("1", "2"), false);
+    });
+    it('should return false when the first argument is "1" and the second is also "12"', function(){      
+    	assert.equal(pn.startsWith("1", "12"), false);
+    });
+    it('should return true when the first argument is "12" and the second is also "1"', function(){      
+    	assert.equal(pn.startsWith("12", "1"), true);
+    });
+    it('should return true when the first argument is "12" and the second is also "1"', function(){      
+    	assert.equal(pn.startsWith("12", ""), true);
+    });
+});
+
+describe('isValidPhoneNumber', function() {
+    it('should return true when the argument is "1"', function(){
+      assert.equal(pn.isValidPhoneNumber("1"), true);
+    });
+
+    it('should return true when the argument is "12"', function(){
+      assert.equal(pn.isValidPhoneNumber("12"), true);
+    });
+
+    it('should return false when the argument is "1a"', function(){
+      assert.equal(pn.isValidPhoneNumber("1a"), false);
+    });
+});
+
+
+describe('isConsistentNumberList', function() {
+    it('should return true when the argument is ["1", "2"]', function(){
+      assert.equal(pn.isConsistentNumberList(["1", "2"]), true);
+    });
+    it('should return false when the argument is ["12", "2","1"]', function(){
+      assert.equal(pn.isConsistentNumberList(["12", "2", "1"]), false);
+    });
+    it('should return false when the argument is ["1", "2","12"]', function(){
+      assert.equal(pn.isConsistentNumberList(["1", "2", "12"]), false);
+    });
+});
+
+
+describe('getEmptyPartitionedList', function() {
+	it('should return [[], [], [], [], [], [], [], [], [], []]', function(){
+      assert.deepEqual(pn.getEmptyPartitionedList(), [[], [], [], [], [], [], [], [], [], []]);
+    });
+});
+
+describe('partitionList', function() {
+	it('should return [["1"], ["2"], ["3"]] when the argument is ["1", "2", "3"]', function(){
+      assert.deepEqual(pn.partitionList(["1", "2", "3"]), [["1"], ["2"], ["3"]]);
+    });
+});
