32e3580d785ea4e8fee64385fc8b43d389457a00
nielsmadan <nielsmadan@gmail.com>
2013-06-18 11:00:10 +0200

    python: partial solution to sudoku board generation.

--- python/sudoku/sudoku.py
+++ python/sudoku/sudoku.py
@@ -1,5 +1,62 @@
+BOARD_SIZE = 9
 
 
+def create_board():
+    return [[0] * BOARD_SIZE] * BOARD_SIZE
 
-def foo(x):
-    return x * x
+
+def print_board(board):
+    print '\n'.join(['%s' % row for row in board])
+
+
+def valid_for_row(row, number_to_check):
+    return number_to_check not in row
+
+
+def valid_for_column(column, number_to_check):
+    return number_to_check not in column
+
+
+def valid_for_block(block, number_to_check):
+    return number_to_check not in block
+
+
+def available_numbers(seq):
+    return [num for num in range(1, 10) if num not in seq]
+
+
+def extract_column(board, index):
+    return [row[index] for row in board]
+
+
+def extract_block(board, blockX, blockY):
+    row_offset = blockY * 3
+    column_offset = blockX * 3
+    block_size = 3
+    block = []
+
+    for row_index in range(row_offset, row_offset + block_size):
+        block.extend(board[row_index][column_offset:column_offset+block_size])
+
+    return block
+
+
+def find_block_index(x, y):
+    return x / 3, y / 3
+
+
+def valid_board(
+
+
+# def generate_sudoku():
+#     board = create_board()
+# 
+#     return generate_sudoku_recurse(board, 0, 0)
+# 
+# 
+# def generate_sudoku_recurse(board, x, y):
+#     pass
+
+
+if __name__ == "__main__":
+    print_board(create_board())
--- python/sudoku/test_sudoku.py
+++ python/sudoku/test_sudoku.py
@@ -1,8 +1,56 @@
-import nose
 from nose.tools import eq_
 
 import sudoku
 
 
-def test_foo():
-    eq_(sudoku.foo(5), 24)
+def check_valid_board(board):
+    for row in board:
+        pass
+
+
+def test_check_valid_board():
+    pass
+
+
+def test_create_board():
+    eq_(sudoku.create_board(), [[0] * 9] * 9)
+
+
+def test_valid_for_row():
+    eq_(sudoku.valid_for_row([], 1), True)
+
+
+def test_valid_for_row_number_already_exists():
+    eq_(sudoku.valid_for_row([1], 1), False)
+
+
+def test_valid_for_column():
+    eq_(sudoku.valid_for_column([], 1), True)
+
+
+def test_valid_for_block():
+    eq_(sudoku.valid_for_block([], 1), True)
+
+
+def test_extract_column():
+    eq_(sudoku.extract_column([[1] + [0] * 8] * 9, 0), [1] * 9)
+
+
+def test_available_numbers_none():
+    eq_(sudoku.available_numbers(range(1, 10)), [])
+
+
+def test_available_numbers_all():
+    eq_(sudoku.available_numbers([]), range(1, 10))
+
+
+def test_available_numbers_some():
+    eq_(sudoku.available_numbers(range(1, 5)), range(5, 10))
+
+
+def test_find_block_index():
+    eq_(sudoku.find_block_index(5, 5), (1, 1))
+
+
+def test_extract_block():
+    eq_(sudoku.extract_block([[1] + [0] * 8] * 9, 0, 0), [1, 0, 0] * 3)
