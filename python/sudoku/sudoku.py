0dbe1c1f6dc759703f35f0333de38f2e1a5c592d
nielsmadan <nielsmadan@gmail.com>
2013-07-11 09:45:01 +0200

    python: generate a sudoku board.

--- python/sudoku/sudoku.py
+++ python/sudoku/sudoku.py
@@ -1,5 +1,25 @@
-BOARD_SIZE = 9
+import random
 import copy
+
+
+BOARD_SIZE = 9
+
+
+def limit_calls(limit, limit_return=None):
+    count = [0]
+
+    def _inner_decorator(fn):
+        def _inner_function(*args, **kwargs):
+            if count[0] < limit:
+                count[0] += 1
+                return fn(*args, **kwargs)
+            else:
+                return limit_return
+
+        return _inner_function
+
+    return _inner_decorator
+
 
 def create_board():
     board = []
@@ -13,20 +33,20 @@ def print_board(board):
     print '\n'.join(['%s' % row for row in board])
 
 
-def valid_for_row(row, number_to_check):
-    return number_to_check not in row
+def valid_for_seq(row):
+    return [num for num in range(1, 10) if num not in row]
 
 
-def valid_for_column(column, number_to_check):
-    return number_to_check not in column
+def valid_for_block(board, x, y):
+    return valid_for_seq(extract_block(board, x, y))
 
 
-def valid_for_block(block, number_to_check):
-    return number_to_check not in block
+def valid_for_column(board, x):
+    return valid_for_seq(extract_column(board, x))
 
 
-def available_numbers(seq):
-    return [num for num in range(1, 10) if num not in seq]
+def valid_for_row(board, y):
+    return valid_for_seq(board[y])
 
 
 def extract_column(board, index):
@@ -34,65 +54,46 @@ def extract_column(board, index):
 
 
 def extract_block(board, blockX, blockY):
-    row_offset = blockY * 3
-    column_offset = blockX * 3
+    row_offset = blockY - blockY % 3
+    column_offset = blockX - blockX % 3
     block_size = 3
     block = []
 
     for row_index in range(row_offset, row_offset + block_size):
-        block.extend(board[row_index][column_offset:column_offset+block_size])
+        block.extend(board[row_index][column_offset:column_offset + block_size])
 
     return block
 
 
-def find_block_index(x, y):
-    return x / 3, y / 3
-
-def has_next(y, x):
-    if x == BOARD_SIZE - 1 and y == BOARD_SIZE - 1:
-        return False
-    return True
-
-def next(y, x):
-    if x < BOARD_SIZE - 1:
-        return (y, x + 1)
-    elif x == BOARD_SIZE - 1:
-        assert(y < BOARD_SIZE)
-        return (y + 1, 0)
-    else:
-        assert(0)
-
-
 def generate_sudoku():
-    board = create_board()
-    generate_sudoku_recurse(board, 0, 0)
-    return board
+    return generate_sudoku_recurse(create_board())
 
-NITER = 0
-def generate_sudoku_recurse(board, y, x):
-    global NITER
-    NITER += 1
-    print "generate_sudoku_recurse(", y, ", ", x, ")"
-    for i in range(1,10):
-        if valid_for_block(extract_block(board, x/3, y/3), i)\
-            and valid_for_row(board[y], i)\
-            and valid_for_column(extract_column(board, x), i):
 
+def valid_numbers(board, y, x):
+    return [num for num in range(1, 10)
+                if num in valid_for_block(board, x, y) and
+                   num in valid_for_column(board, x) and
+                   num in valid_for_row(board, y)]
+
+
+# @limit_calls(18, True)
+def generate_sudoku_recurse(board):
+    if board[BOARD_SIZE - 1][BOARD_SIZE - 1] != 0:
+        return board
+    else:
+        y, x = [(y, x) for y, row in enumerate(board)
+                       for x, column in enumerate(row) if board[y][x] == 0][0]
+
+        valid_nums = valid_numbers(board, y, x)
+        random.shuffle(valid_nums)
+        for num in valid_nums:
             newboard = copy.deepcopy(board)
-            print "found: ", i, "NITER: ", NITER
-            newboard[y][x] = i
-            print_board(newboard)
-            if has_next(y, x):
-                if generate_sudoku_recurse(newboard, *next(y, x)):
-                    board = newboard
-                    return True
-                 
-            else:
-                board = newboard
-                return True
-    return False
+            newboard[y][x] = num
+
+            nxt = generate_sudoku_recurse(newboard)
+            if nxt is not None:
+                return nxt
 
 
 if __name__ == "__main__":
-    #generate_sudoku()
     print_board(generate_sudoku())
--- python/sudoku/test_sudoku.py
+++ python/sudoku/test_sudoku.py
@@ -3,67 +3,69 @@ from nose.tools import eq_
 import sudoku
 
 
+def valid_seq(seq):
+    return len(set(seq)) == 9
+
+
+def valid_row(board, y):
+    return valid_seq(board[y])
+
+
+def valid_column(board, x):
+    return valid_seq(sudoku.extract_column(board, x))
+
+
+def valid_block(board, x, y):
+    return valid_seq(sudoku.extract_block(board, x, y))
+
+
 def check_valid_board(board):
-    for row in board:
-        pass
-
-
-def test_check_valid_board():
-    pass
+    return (all([valid_row(board, num) and valid_column(board, num) for num in range(9)]) and
+            all([valid_block(board, x * 3, y * 3) for x in range(3) for y in range(3)]))
 
 
 def test_create_board():
     eq_(sudoku.create_board(), [[0] * 9] * 9)
     b = sudoku.create_board()
     b[0][1] = 1
-    eq_(b, [[0,1,0,0,0,0,0,0,0], [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9])
-
-def test_board_size():
-    pass
-
-def test_valid_for_row():
-    eq_(sudoku.valid_for_row([], 1), True)
+    eq_(b, [[0, 1, 0, 0, 0, 0, 0, 0, 0]] + [[0] * 9] * 8)
 
 
-def test_valid_for_row_number_already_exists():
-    eq_(sudoku.valid_for_row([1], 1), False)
+def test_valid_for_seq_none():
+    eq_(sudoku.valid_for_seq([]), range(1, 10))
 
 
-def test_valid_for_column():
-    eq_(sudoku.valid_for_column([], 1), True)
+def test_valid_for_seq_number_single():
+    eq_(sudoku.valid_for_seq([1]), range(2, 10))
 
 
-def test_valid_for_block():
-    eq_(sudoku.valid_for_block([], 1), True)
+def test_valid_for_seq_number_some():
+    eq_(sudoku.valid_for_seq([1, 5, 7, 9]), [2, 3, 4, 6, 8])
+
+
+def test_valid_for_seq_number_all():
+    eq_(sudoku.valid_for_seq(range(1, 10)), [])
 
 
 def test_extract_column():
     eq_(sudoku.extract_column([[1] + [0] * 8] * 9, 0), [1] * 9)
 
 
-def test_available_numbers_none():
-    eq_(sudoku.available_numbers(range(1, 10)), [])
-
-
-def test_available_numbers_all():
-    eq_(sudoku.available_numbers([]), range(1, 10))
-
-
-def test_available_numbers_some():
-    eq_(sudoku.available_numbers(range(1, 5)), range(5, 10))
-
-
-def test_find_block_index():
-    eq_(sudoku.find_block_index(5, 5), (1, 1))
-
-
 def test_extract_block():
     eq_(sudoku.extract_block([[1] + [0] * 8] * 9, 0, 0), [1, 0, 0] * 3)
-    eq_(sudoku.extract_block([[0,1,0,0,0,0,0,0,0], [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9], 0, 0),
-            [0,1,0,0,0,0,0,0,0])
-    eq_(sudoku.extract_block([[0,0,0,0,0,0,0,0,1], [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9], 2, 0),
-            [0,0,1,0,0,0,0,0,0])
 
-def test_generate_sudoku():
-    print "KK"
-    #print sudoku.generate_sudoku()
+    eq_(sudoku.extract_block([[0, 1, 0, 0, 0, 0, 0, 0, 0],
+                              [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9], 0, 0),
+        [0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+    eq_(sudoku.extract_block([[0, 0, 0, 0, 0, 0, 0, 0, 1],
+                              [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9], 7, 0),
+        [0, 0, 1, 0, 0, 0, 0, 0, 0])
+
+
+def test_valid_for_row():
+    eq_(sudoku.valid_for_row([[0] * 9, [0, 1, 3, 2, 0, 0, 0, 0, 0]] + [[0] * 9] * 7, 1), [4, 5, 6, 7, 8, 9])
+
+
+def test_generate_board():
+    eq_(check_valid_board(sudoku.generate_sudoku()), True)
