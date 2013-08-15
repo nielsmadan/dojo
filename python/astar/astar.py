339b4ec80a5900e18d4c4be03ecda547691242d3
nielsmadan <nielsmadan@gmail.com>
2013-08-15 17:44:35 +0200

    python: finish dijkstra

--- python/astar/astar.py
+++ python/astar/astar.py
@@ -1,5 +1,9 @@
 from collections import defaultdict
+from Queue import PriorityQueue, Empty
 
+import sys
+
+infinity = sys.maxint
 
 GRAPH_STRING = [
     "---------------------",
@@ -14,65 +18,113 @@ GRAPH_STRING = [
 ]
 
 
+def get_neighbors(node_list, y, x):
+    result = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if ((y + i) >= 0 and (y + i) < len(node_list) and
+                    (x + j) >= 0 and (x + j) < len(node_list[0]) and
+                    not (i == 0 and j == 0)):
+                result.append(node_list[y + i][x + j])
+
+    return result
+
+
 class Graph(object):
-    def __init__(self, node_dict):
-        self.node_dict = node_dict
+    def __init__(self, node_dict=None, start=None, target=None):
+        if node_dict is not None:
+            self.node_dict = node_dict
+            self.start_node = start
+            self.target_node = target
+        else:
+            self.node_dict = defaultdict(list)
+            self.start_node = None
+            self.target_node = None
+
+    def add_node(self, node, neighbors):
+        self.node_dict[node].extend(neighbors)
+
+    def get_neighbors(self, node):
+        return self.node_dict[node]
+
+    @staticmethod
+    def find_node(node_list, string_repr, search_type):
+        for y, node_row in enumerate(string_repr):
+            for x, node in enumerate(node_row):
+                if string_repr[y][x] == search_type:
+                    return node_list[y][x]
+
+        assert False
 
     @staticmethod
     def from_string(string_graph):
-        def get_node(c):
+        def create_node(c):
             if c is not "X":
-                return Node(c == 'S', c == 'T')
+                return Node()
 
-        def get_neighbors(node_list, y, x):
-            return [node_list[y + i][x + j] for i in range(-1, 2)
-                                            for j in range(-1, 2)
-                                                if (y + i) >= 0 and
-                                                   (y + i) < len(node_list) and
-                                                   (x + i) >= 0 and
-                                                   (x + i) < len(node_list[0]) and
-                                                   (i != 0 and j != 0)]
+        node_list = [[create_node(c) for c in row] for row in string_graph]
 
-        node_list = [[Node(get_node(c)) for c in row] for row in string_graph]
-        node_dict = {}
+        start_node = Graph.find_node(node_list, string_graph, 'S')
+        target_node = Graph.find_node(node_list, string_graph, 'T')
+
+        node_dict = defaultdict(list)
         for y, row in enumerate(node_list):
             for x, node in enumerate(row):
                 if node is not None:
                     node_dict[node] = get_neighbors(node_list, y, x)
 
-        return Graph(node_dict)
+        return Graph(node_dict, start_node, target_node)
 
 
 class Node(object):
-    def __init__(self, start=False, target=False):
-        self.is_start = start
-        self.is_target = target
-
-
-def find_start_node(graph):
-    return next(node for node in graph.node_dict.keys() if node.is_start)
+    def __init__(self, x=None, y=None):
+        self.x = x
+        self.y = y
 
 
 def find_target_node(graph):
     return next(node for node in graph.node_dict.keys() if node.is_target)
 
 
-# def dijkstra(graph):
-#     graph = assign_initial_distance(graph)
-#     # graph = mark_initial_graph_unvisited(graph)
-#
-#     start_node = find_start(graph)
-#     target_node = find_target(graph)
-#
-#     current_node = start_node
-#     visited_nodes = []
-#
-#     while current_node is not None:
-#         graph = calculate_neighbor_distances(current_node, graph, visited_nodes)
-#
-#         visited_nodes = mark_as_visited(current_node, visited_nodes)
-#
-#         if is_visited(target_node, unvisited_nodes):
-#             return
-#
-#         current_node = find_smallest_distance_node(current_node, visited, graph)
+def find_ancestors(ancestors_dict, leaf_node, root_node):
+    current_node = leaf_node
+    result_list = [current_node]
+
+    while current_node != root_node:
+        current_node = ancestors_dict[current_node]
+        result_list.append(current_node)
+
+    assert result_list[-1] == root_node
+
+    return result_list
+
+
+def dijkstra(graph, start_node, target_node):
+    frontier = PriorityQueue()
+
+    current_node = start_node
+    distance_dict = defaultdict(lambda: infinity)
+    distance_dict[current_node] = 0
+    ancestors_dict = {}
+    visited_set = set()
+
+    while True:
+        neighbors = graph.get_neighbors(current_node)
+
+        current_distance = distance_dict[current_node]
+
+        for neighbor in neighbors:
+            if neighbor not in visited_set and (current_distance + 1) < distance_dict[neighbor]:
+                distance_dict[neighbor] = current_distance + 1
+                ancestors_dict[neighbor] = current_node
+                frontier.put((distance_dict[neighbor], neighbor))
+
+        visited_set.add(current_node)
+
+        if current_node == target_node:
+            return list(reversed(find_ancestors(ancestors_dict, current_node, start_node)))
+        else:
+            try:
+                current_node = frontier.get_nowait()[1]
+            except Empty:
+                break
--- python/astar/test_astar.py
+++ python/astar/test_astar.py
@@ -2,51 +2,137 @@ import astar
 from nose.tools import eq_, ok_
 
 FULL_TEST_GRAPH = (
-    "---------------------"
-    "------------------T--"
-    "----XXXXXXXXXX-------"
-    "-------------X-------"
-    "-------------X-------"
-    "------S------X-------"
-    "-------------X-------"
-    "-------------X-------"
-    "---------------------"
+    "---------------------",
+    "------------------T--",
+    "----XXXXXXXXXX-------",
+    "-------------X-------",
+    "-------------X-------",
+    "------S------X-------",
+    "-------------X-------",
+    "-------------X-------",
+    "---------------------",
 )
 
 
-def test_find_start_single_node():
-    graph = astar.Graph.from_string(["S"])
-    ok_(astar.find_start_node(graph).is_start)
+def test_find_node_row():
+    node_a = astar.Node()
+    node_b = astar.Node()
+
+    result = astar.Graph.find_node([[node_a, node_b]], ["-S"], 'S')
+
+    eq_(result, node_b)
 
 
-def test_find_start_single_row():
-    graph = astar.Graph.from_string(["---S---"])
-    ok_(astar.find_start_node(graph).is_start)
+def test_find_node_column():
+    node_a = astar.Node()
+    node_b = astar.Node()
 
+    result = astar.Graph.find_node([[node_a], [node_b]], ["-", "S"], 'S')
 
-def test_find_start_multiple_row():
-    graph = astar.Graph.from_string(["------", "---S---", "------"])
-    ok_(astar.find_start_node(graph).is_start)
-
-
-def test_find_target_single_node():
-    graph = astar.Graph.from_string(["T"])
-    ok_(astar.find_target_node(graph).is_target)
-
-
-def test_find_target_single_row():
-    graph = astar.Graph.from_string(["---T---"])
-    ok_(astar.find_target_node(graph).is_target)
-
-
-def test_find_target_multiple_row():
-    graph = astar.Graph.from_string(["------", "---T---", "------"])
-    ok_(astar.find_target_node(graph).is_target)
+    eq_(result, node_b)
 
 
 def test_get_graph():
-    graph = astar.Graph.from_string(["-", "-"])
+    graph = astar.Graph.from_string(["T",
+                                     "S"])
 
     node_keys = graph.node_dict.keys()
     eq_(graph.node_dict[node_keys[0]], [node_keys[1]])
     eq_(graph.node_dict[node_keys[1]], [node_keys[0]])
+
+
+def test_get_neighbors_one_row_neighbor():
+    node_a = astar.Node()
+    node_b = astar.Node()
+
+    neighbor_list = astar.get_neighbors([[node_a, node_b]], 0, 0)
+    eq_(neighbor_list, [node_b])
+
+    neighbor_list = astar.get_neighbors([[node_a, node_b]], 0, 1)
+    eq_(neighbor_list, [node_a])
+
+
+def test_get_neighbors_one_column_neighbor():
+    node_a = astar.Node()
+    node_b = astar.Node()
+
+    neighbor_list = astar.get_neighbors([[node_a], [node_b]], 0, 0)
+    eq_(neighbor_list, [node_b])
+
+    neighbor_list = astar.get_neighbors([[node_a], [node_b]], 1, 0)
+    eq_(neighbor_list, [node_a])
+
+
+def test_find_ancestors():
+    input_dict = {
+        'b': 'a',
+        'c': 'b',
+    }
+
+    result = astar.find_ancestors(input_dict, 'c', 'a')
+
+    eq_(result, ['c', 'b', 'a'])
+
+
+def test_find_ancestors_two_levels():
+    input_dict = {
+        'b': 'a',
+        'c': 'b',
+        'd': 'c',
+    }
+
+    result = astar.find_ancestors(input_dict, 'd', 'a')
+
+    eq_(result, ['d', 'c', 'b', 'a'])
+
+
+def test_dijkstra_two_node_graph():
+    graph = astar.Graph.from_string("ST")
+
+    result = astar.dijkstra(graph, graph.start_node, graph.target_node)
+
+    eq_(result, [graph.start_node, graph.target_node])
+
+
+def test_dijkstra_three_node_graph():
+    start_node = astar.Node()
+    target_node = astar.Node()
+    middle_node = astar.Node()
+
+    graph = astar.Graph()
+
+    graph.add_node(start_node, [middle_node])
+    graph.add_node(middle_node, [start_node, target_node])
+    graph.add_node(target_node, [middle_node])
+
+    result = astar.dijkstra(graph, start_node, target_node)
+
+    eq_(result, [start_node, middle_node, target_node])
+
+
+def test_dijkstra_impossible():
+    start_node = astar.Node()
+    target_node = astar.Node()
+    middle_node = astar.Node()
+
+    graph = astar.Graph()
+
+    graph.add_node(start_node, [middle_node])
+    graph.add_node(middle_node, [start_node])
+    graph.add_node(target_node, [])
+
+    result = astar.dijkstra(graph, start_node, target_node)
+
+    eq_(result, None)
+
+
+def test_dijkstra_complex_graph():
+    graph = astar.Graph.from_string(FULL_TEST_GRAPH)
+
+    result = astar.dijkstra(graph, graph.start_node, graph.target_node)
+
+    eq_(result[0], graph.start_node)
+    eq_(result[-1], graph.target_node)
+
+    # for i, node in enumerate(result[:-1]):
+    #     eq_(graph.node_dict[node]
