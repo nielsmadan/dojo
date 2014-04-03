151558ca3365b912296dc7a709475e2c735f57ee
nielsmadan <nielsmadan@gmail.com>
2014-04-03 17:16:08 +0200

    rust: finish game of life

--- rust/game_of_life.rs
+++ rust/game_of_life.rs
@@ -32,15 +32,72 @@ fn get_alive_neighbors(cell: (int, int), board: ~[(int, int)]) -> int {
     return ret;
 }
 
-fn is_alive(cell: (int, int), board: ~[(int, int)]) -> bool {
+fn will_be_alive(cell: (int, int), board: ~[(int, int)]) -> bool {
+    let b = board.contains(&cell);
+
     let n = get_alive_neighbors(cell, board);
 
-    return n >= 2 && n <= 3
+    return (n == 3) || ((n == 2) && b);
 }
 
 fn find_candidate_cells(cells: ~[(int, int)]) -> ~[(int, int)] {
-    let cell: (int, int) = cells[0];
-    return get_neighbors(cell);
+    let mut ret: ~[(int, int)] = ~[];
+
+    for cell in cells.iter() {
+        let n = get_neighbors(*cell);
+
+        for x in n.iter() {
+            if (!ret.contains(x)) {
+                ret.push(*x);
+            }
+        }
+    }
+
+    return ret;
+}
+
+//fn filtered<T>(lst: ~[T], predicate: |&T| -> bool) -> ~[T] {
+//    let mut ret: ~[T] = ~[];
+//
+//    for elem in lst.iter() {
+//        if (predicate(elem)) {
+//            let el = elem.clone();
+//            ret.push(*el);
+//        }
+//    }
+//
+//    return ret;
+//}
+//
+//fn from_iter<T>(it: ~Iterator<T>) -> ~[T] {
+//    let mut ret = ~[];
+//
+//    let n: Option<T> = it.next();
+//
+//    while (n.is_some()) {
+//        ret.push(n.unwrap());
+//        n = it.next();
+//    }
+//
+//    return ret;
+//}
+
+fn next_gen(cells: ~[(int, int)]) -> ~[(int, int)] {
+    let mut ret: ~[(int, int)] = ~[];
+
+    let candidates = find_candidate_cells(cells.clone());
+
+    //return filtered(candidates, |&x| will_be_alive(x, cells.clone()));
+    // let mut it = a.iter().map(|&x| 2 * x);
+    //return from_iter(candidates.iter().filter(|&x| will_be_alive(*x, cells.clone())));
+
+    for cell in candidates.iter() {
+        if (will_be_alive(*cell, cells.clone())) {
+            ret.push(*cell);
+        }
+    }
+
+    return ret;
 }
 
 #[test]
@@ -64,8 +121,28 @@ fn test_get_alive_neighbors() {
 }
 
 #[test]
-fn test_is_alive() {
-    assert_eq!(is_alive((0, 0), ~[(0, 0), (1, 1)]), false);
-    assert_eq!(is_alive((0, 0), ~[(0, 0), (1, 1), (-1, -1)]), true);
-    assert_eq!(is_alive((0, 0), ~[(0, 0), (-1, 0), (1, 0), (1, 1), (-1, -1)]), false);
+fn test_wll_be_alive() {
+    assert_eq!(will_be_alive((0, 0), ~[(0, 0), (1, 1)]), false);
+    assert_eq!(will_be_alive((0, 0), ~[(1, 1), (-1, -1)]), false);
+    assert_eq!(will_be_alive((0, 0), ~[(-1, 1), (1, 1), (-1, -1)]), true);
+    assert_eq!(will_be_alive((0, 0), ~[(0, 0), (1, 1), (-1, -1)]), true);
+    assert_eq!(will_be_alive((0, 0), ~[(0, 0), (-1, 0), (1, 0), (1, 1), (-1, -1)]), false);
 }
+
+#[test]
+fn test_next_gen() {
+    assert_eq!(next_gen(~[(0, 0)]), ~[]);
+    assert_eq!(next_gen(~[(0, 0), (0, 1), (1, 0), (1, 1)]), ~[(0, 1), (1, 0), (1, 1), (0, 0)]);
+    assert_eq!(next_gen(~[(1, 0), (1, 1), (1, 2)]), ~[(0, 1), (1, 1), (2, 1)]);
+}
+
+//.x.
+//.x.
+//.x.
+//
+//...
+//xxx
+//...
+
+
+
---
+++ rust/test.sh
@@ -0,0 +1,1 @@
+rustc --test game_of_life.rs -o out/test_game_of_life && ./out/test_game_of_life
